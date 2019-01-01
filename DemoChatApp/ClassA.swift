559132380b4b258bba4ab57c802f5798d83f6e51
Appinventiv <appinventiv@Apples-MBP-4.localdomain>
2019-01-01 16:35:54 +0530

    Open and public files added

---
+++ DemoChatApp/ClassA.swift
@@ -0,0 +1,9 @@
+//
+//  ClassA.swift
+//  DemoChatApp
+//
+//  Created by Appinventiv on 01/01/19.
+//  Copyright © 2019 Vikash. All rights reserved.
+//
+
+import Foundation
---
+++ DemoChatApp/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  DemoChatApp
+//
+//  Created by Appinventiv on 31/12/18.
+//  Copyright © 2018 Vikash. All rights reserved.
+//
+
+import Foundation
